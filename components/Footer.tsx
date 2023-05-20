55c7b59e2957145a9d2d268567878dddcde9031a
Adrian <adekpp@gmail.com>
2023-05-20 15:49:13 +0200

    footer padding

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="flex h-16 flex-none items-center justify-center bg-teal-900 md:rounded-md text-white text-xs md:text-base">
+    <div className="flex h-16 flex-none items-center justify-center bg-teal-900 md:rounded-md text-white text-xs md:text-base px-3">
       <p>Created by Adrian Pietryga | This project is licensed under the MIT License
 </p>
     </div>
