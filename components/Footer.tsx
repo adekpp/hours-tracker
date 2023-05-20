46f74d293b932789f1abecf24d08a437b4b94961
Adrian <adekpp@gmail.com>
2023-05-20 15:46:48 +0200

    footer content update, styles update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
   return (
-    <div className="flex h-16 flex-none items-center justify-center bg-teal-900 md:rounded-md text-white">
-      <p>Adrian Pietryga</p>
+    <div className="flex h-16 flex-none items-center justify-center bg-teal-900 md:rounded-md text-white text-xs md:text-base">
+      <p>Created by Adrian Pietryga | This project is licensed under the MIT License
+</p>
     </div>
   );
 }
