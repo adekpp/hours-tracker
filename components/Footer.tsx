8d227ba658378f86b8843950f031bceadb986f26
Adrian <adekpp@gmail.com>
2023-05-20 16:12:16 +0200

    footer styles added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <div className="flex h-16 flex-none items-center justify-center bg-teal-900 md:rounded-md text-white text-xs md:text-base px-3">
-      <p>Created by Adrian Pietryga</p>
+    <div className="flex flex-col h-16 flex-none items-center justify-center bg-teal-900 md:rounded-md text-white text-xs md:text-base px-3">
+      <p>Created by <Link className="underline font-semibold" href={"https://github.com/adekpp"} target="_blank">Adrian Pietryga</Link></p>
       <p>This project is licensed under the MIT License</p>
 
     </div>
